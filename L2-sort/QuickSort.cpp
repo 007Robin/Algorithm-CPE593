a31ea3554d6c2ebdc8a53e2bad6d48f036ab8220
007Robin <30958079+007Robin@users.noreply.github.com>
2020-12-05 17:19:52 -0500

    Update QuickSort.cpp

--- L2-sort/QuickSort.cpp
+++ L2-sort/QuickSort.cpp
@@ -3,8 +3,9 @@
 #include<fstream>
 #include<vector>
 using namespace std;
-
-//get a pivot, sort once
+//O(n log n) 
+//步骤解释： https://docs.google.com/document/d/1qYlmVRfTxceHM6tBQjUbWOQnUslhx4qnJ8TzU1OVMBI/edit?usp=sharing
+//get a pivot, sort once, 经过一次sort划出，左边是比pivot小的一些数，右边是比pivot大的数
 void SortOnce(vector<int>&v, int beg, int end) {
 	// get a pivot
 	if(beg >= end) return ;
@@ -15,12 +16,14 @@ void SortOnce(vector<int>&v, int beg, int end) {
 	int start = beg;
 	int p = beg;
 	while(p < end) {
+		//用p辅助遍历，遇到比pivot小的数，swap v[p]与v[start], start总track小的数
 		if(v[p] < pivot) {
 			swap(v[start], v[p]);
 			start++;
 		}
 		p++;
 	}
+	//最后相当于swap v[start]与pivot，相当于把pivot放在了它应该在的位置，在这个位置左边都比它小，右边都比它大
 	swap(v[start], v[p]);
 	
 	SortOnce(v, beg, start - 1);
